d304011c830a1e9757dc4434f28084ccecafd6a5
Shaan Sharma <shaansharma@outlook.com>
2015-09-12 02:15:14 -0400

    project created

---
+++ Julius/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Julius
+//
+//  Created by Shaan Sharma on 2015-09-12.
+//  Copyright (c) 2015 Shaan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
